26f7129c15151738e6e58c6d285fd940c2e0cffd
iehtian <1850453937@qq.com>
2025-09-26 18:40:24 +0800

    修复因日期导致的时间错误

--- assert/js/myjs.js
+++ assert/js/myjs.js
@@ -35,7 +35,11 @@ function generateTimeIntervalsSimple() {
 const time_slots = generateTimeIntervalsSimple()
 
 function getCurrentDateISO() {
-  return new Date().toISOString().split("T")[0]
+  const date = new Date()
+  const year = date.getFullYear()
+  const month = String(date.getMonth() + 1).padStart(2, "0") // 注意：月份从0开始
+  const day = String(date.getDate()).padStart(2, "0")
+  return `${year}-${month}-${day}`
 }
 
 function getCurrentTimeSlotIndex() {
