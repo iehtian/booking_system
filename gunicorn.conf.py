23fe11297de0fb937e8bb025eb47798c4f07eba0
iehtian <1850453937@qq.com>
2025-10-09 23:18:34 +0800

    修复因log而导致的自动刷新

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,6 @@
 bind = '0.0.0.0:5000'
 timeout = 120
-accesslog = 'logs/access.log'
-errorlog = 'logs/error.log'
+# 将日志写到站点根目录之外，避免与前端静态资源目录混杂
+accesslog = '/var/log/order/access.log'
+errorlog = '/var/log/order/error.log'
 loglevel = 'info'
